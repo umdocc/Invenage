c4a98a4375d40cfb2ac1401e71474c8b80d4cf57
Cuong Do <cuongcaodo@gmail.com>
2019-11-04 16:39:10 +0700

    Update build_master_db.py

--- python/build_master_db.py
+++ python/build_master_db.py
@@ -1,7 +1,6 @@
 # check the database for missing actual cost price and attemp to fill
 # ---------------------- Setup Block ------------------------------------------
 import sys, os, pandas as pd, sqlite3
-from shutil import copy
 sys.path.append(os.path.join(os.path.expanduser('~'),'invenage_data'))
 from python_conf import create_config_dict
 config_dict = create_config_dict()
@@ -12,8 +11,6 @@ inv_db = os.path.join(home_path,'Dropbox','MDSCloud','SQLite',
 master_db = os.path.join(home_path,'Documents','MDS',
                                            'mds-master.sqlite')
 
-copy(inv_db,master_db)
-
 # a dictionary to figure out where to find what
 extra_df = pd.read_csv(os.path.join(
         home_path,'Documents','MDS','extra_data_info.csv'))
@@ -23,9 +20,21 @@ for i in range(0,len(extra_df)):
     
 invoice_1518 = pd.read_excel(ext_data_dict['invoice_1518'])
 invoice_19 = pd.read_excel(ext_data_dict['invoice_19'], skiprows=2, dtype=str)
-mconn = sqlite3.connect(master_db)
-mconn.close()
+
 
 # spn
 tmp = invoice_1518.copy()
 tmp = tmp[tmp.vendor_id==7]
+
+# this section read tables from various databases and writing to master
+
+# ------------------ read tables from inventory -------------------------------
+iconn = sqlite3.connect(inv_db)
+sale_log = pd.read_sql_query('select * from sale_log',iconn)
+import_log = pd.read_sql_query('select * from import_log',iconn)
+iconn.close()
+
+mconn = sqlite3.connect(master_db)
+sale_log.to_sql('sale_log',mconn,index=False,if_exists='replace')
+import_log.to_sql('import_log',mconn,index=False,if_exists='replace')
+mconn.close()
