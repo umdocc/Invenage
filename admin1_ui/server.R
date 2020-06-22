a3a5477ae3678b93f5ad386abcf686270daa2d8b
umdocc <cuongcaodo@gmail.com>
2020-06-22 09:37:27 +0700

    remove global.R
    integrate to server.R

--- admin1_ui/server.R
+++ admin1_ui/server.R
@@ -1,7 +1,19 @@
-# Invenage server.R
-source("global.R",local = F)
 require(dplyr)
 require(DT)
+# --------------------------- start up script ----------------------------------
+rm(list=ls()) # boot clean-up
+options(warn=-1) # supress warnings
+app_path <- dirname(getwd()) # app_path is one level up from shiny folder
+# print(app_path)
+boot_path <- file.path(app_path,'r','boot.R')
+
+# point to local file as app_path will be invalid during dev
+if(!file.exists(boot_path)){
+  app_path <- '~/Documents/GitHub/Invenage/'
+}
+source(file.path(app_path,'r','boot.R'))
+
+# server
 shinyServer(function(input, output,session) {
   session$onSessionEnded( function(){
     stopApp()
