dcd7810b50ea5dc607960ab0594aa35a54278009
umdocc <cuongcaodo@gmail.com>
2020-06-24 17:10:34 +0700

    remove admin2

--- admin2_ui/global.R
+++
@@ -1,5 +0,0 @@
-# the global script main purpose is to read boot.R
-rm(list=ls()) # boot clean-up
-options(warn=-1) # supress warnings
-app_path <- dirname(getwd()) # app_path is one level up from shiny folder
-source(file.path(app_path,'r','boot.R'))
