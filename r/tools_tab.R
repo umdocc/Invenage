28e9c6f7e08ba395c8cd87114ce93bf28410c93d
umdocc <cuongcaodo@gmail.com>
2020-08-05 10:39:02 +0700

    added tools
    various tools for interfacing with excel

---
+++ r/tools_tab.R
@@ -0,0 +1,21 @@
+# provide various tools
+
+po_name <- 'MDS.Biolabo.PO.280720'
+compare_po_invoice(po_name,invoice_excel_name){
+  po_data <- read_excel_po(get_po_filepath(po_name,config_dict))
+  
+  # fix for code still using vendor
+  if (!('vendor_id' %in% names(po_data))){
+    po_data <- merge(po_data,vendor_info %>% select(vendor,vendor_id))
+    po_data$vendor <- NULL
+  }
+  
+  # get the prod_code
+  if (!('prod_code' %in% names(po_data))){
+    po_data <- merge(po_data,
+                     product_info %>% select(prod_code,vendor_id,ref_smn))
+  }
+  po_data <- get_import_price(po_data)
+}
+
+
