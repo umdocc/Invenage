179469a95c27aa862d3adc9a09ef954a4b3fe97b
umdocc <cuongcaodo@gmail.com>
2020-08-05 16:40:22 +0700

    update tools tab

--- r/tools_tab.R
+++ r/tools_tab.R
@@ -1,6 +1,7 @@
 # provide various tools
 
 po_name <- 'MDS.Biolabo.PO.280720'
+po_path <- get_po_filepath(po_name,config_dict)
 compare_po_invoice(po_name,invoice_excel_name){
   po_data <- read_excel_po(get_po_filepath(po_name,config_dict))
   
@@ -13,9 +14,14 @@ compare_po_invoice(po_name,invoice_excel_name){
   # get the prod_code
   if (!('prod_code' %in% names(po_data))){
     po_data <- merge(po_data,
-                     product_info %>% select(prod_code,vendor_id,ref_smn))
+                     product_info %>% select(prod_code,vendor_id,ref_smn),
+                     all.x=T)
+    
   }
   po_data <- get_import_price(po_data)
 }
+
+
+
 
 
