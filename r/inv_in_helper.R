6cc0dea90b187838f3142a08e5467f7d8f99a337
umdocc <cuongcaodo@gmail.com>
2020-09-04 16:15:46 +0700

    Update inv_in_helper.R
    fix error detection logic

--- r/inv_in_helper.R
+++ r/inv_in_helper.R
@@ -2,15 +2,17 @@
 read_excel_po <- function(
   full_file_path,search_str = 'Description', search_col = 2){
   out_data <- read_excel_table(full_file_path, search_str, search_col)
-  out_data <- col_name_to_label(config_dict,out_data)
-  out_data <- out_data[!is.na(out_data$ref_smn),]
-  out_data$vendor <- get_vendor_from_filename(config_dict, full_file_path)
-  out_data$po_name <- gsub('\\.xlsx','',basename(full_file_path))
-  # ref_smn needs to be string
-  out_data$ref_smn <- as.character(out_data$ref_smn)
-  out_data <- out_data %>% 
-    select(
-      stt,name,qty,ref_smn,lot,exp_date,actual_unit_cost,note,vendor,po_name)
+  if(is.data.frame(out_data)){
+    out_data <- col_name_to_label(config_dict,out_data)
+    out_data <- out_data[!is.na(out_data$ref_smn),]
+    out_data$vendor <- get_vendor_from_filename(config_dict, full_file_path)
+    out_data$po_name <- gsub('\\.xlsx','',basename(full_file_path))
+    # ref_smn needs to be string
+    out_data$ref_smn <- as.character(out_data$ref_smn)
+    out_data <- out_data %>% 
+      select(
+        stt,name,qty,ref_smn,lot,exp_date,actual_unit_cost,note,vendor,po_name)
+  }
   return(out_data)
 }
 
@@ -18,7 +20,13 @@ read_excel_po <- function(
 read_excel_table <- function(
   full_file_path,search_str = 'Description', search_col = 2){
   start_pt <- get_excel_tbl_startrow(full_file_path,search_str, search_col)
-  out_data <- read.xlsx(full_file_path, startRow = start_pt+1, detectDates = T)
+  if(length(start_pt)==0){
+    show_alert('error','tbl_start_pt_not_found','error')
+    out_data <- 'error'
+  }else{
+    out_data <- read.xlsx(
+      full_file_path, startRow = start_pt+1, detectDates = T)
+  }
   return(out_data)
 }
 
@@ -48,89 +56,90 @@ load_po_to_db <- function(po_name,config_dict){
   # read the po data
   full_path <- get_po_filepath(po_name,config_dict)
   po_data <- read_excel_po(full_path)
-  
-  # add note columns if not presented in the po
-  if (!('note' %in% names(po_data))){
-    po_data$note <- ''
-  }
-  
-  po_data <- merge(po_data,product_info %>% select(ref_smn,vendor,prod_code),
-                   all.x=T)
-  
-  #remove qty = 0 items and items with no lot
-  po_data <- po_data[po_data$qty >0,]
-  po_data <- po_data[!is.na(po_data$lot) | po_data$lot=='',]
-  
-  # remove the "'" in lot/date
-  po_data$lot <- gsub("'","",po_data$lot)
-  po_data$exp_date <- gsub("'","",po_data$exp_date)
-  
-  # at this stage, proceed if only we have data
-  if (nrow(po_data)>0){
-    # append other information
-    po_data$delivery_date <- Sys.Date() # delivery_date
-    po_data$actual_currency_code <- 1
-    po_data$warehouse_id <- 1
-    po_data <- merge(po_data,vendor_info,all.x=T)
+  if(is.data.frame(po_data)){
+    # add note columns if not presented in the po
+    if (!('note' %in% names(po_data))){
+      po_data$note <- ''
+    }
     
-    # add unit
-    ordering_unit <- get_ordering_unit(packaging)
-    ordering_unit <- ordering_unit[!duplicated(ordering_unit$prod_code),]
-    po_data <- merge(po_data,ordering_unit, all.x=T)
+    po_data <- merge(po_data,product_info %>% select(ref_smn,vendor,prod_code),
+                     all.x=T)
     
-    po_data <- po_data %>% 
-      select(prod_code,unit,qty,po_name,lot,exp_date,actual_unit_cost,
-             actual_currency_code,delivery_date,warehouse_id,vendor_id,note)
+    #remove qty = 0 items and items with no lot
+    po_data <- po_data[po_data$qty >0,]
+    po_data <- po_data[!is.na(po_data$lot) | po_data$lot=='',]
     
+    # remove the "'" in lot/date
+    po_data$lot <- gsub("'","",po_data$lot)
+    po_data$exp_date <- gsub("'","",po_data$exp_date)
     
-    
-    # check and remove existing entries
-    po_data <- check_exist(po_data,import_log, 
-                           check_col = c('prod_code','qty','lot','po_name'))
-    
-    # create a copy to update price, then drop existing entries
-    po_price <- po_data
-    po_data <- po_data[!po_data$exist,]
-    po_data$exist <- NULL
-    
-    
-    # writing to database
+    # at this stage, proceed if only we have data
     if (nrow(po_data)>0){
-      print('writing to import_log'); print(po_data)
-      db_write(config_dict,'import_log',po_data)
-      out_msg <- paste0(
-        out_msg, '\n',ui_elem$actual[ui_elem$label=='add_lotdate_success'])
-    }
-    
-    #update price
-    # keep only rows with price to prevent writing NA in database
-    po_price <- po_price[!is.na(po_price$actual_unit_cost),]
-    if (nrow(po_price)>0){
-      print('updating price(s)'); print(po_price)
-      conn <- db_open(config_dict)
-      for (i in 1:nrow(po_price)){
-        query <- paste0('update import_log set actual_unit_cost = ',
-                        po_price$actual_unit_cost[i],
-                        ' where po_name like ','"',po_price$po_name[i],'"',
-                        ' AND qty = ',po_price$qty[i], 
-                        ' AND lot like ','"',po_price$lot[i],'"',
-                        ' AND prod_code like ','"',po_price$prod_code[i],'"')
-        dbExecute(conn,query)
+      # append other information
+      po_data$delivery_date <- Sys.Date() # delivery_date
+      po_data$actual_currency_code <- 1
+      po_data$warehouse_id <- 1
+      po_data <- merge(po_data,vendor_info,all.x=T)
+      
+      # add unit
+      ordering_unit <- get_ordering_unit(packaging)
+      ordering_unit <- ordering_unit[!duplicated(ordering_unit$prod_code),]
+      po_data <- merge(po_data,ordering_unit, all.x=T)
+      
+      po_data <- po_data %>% 
+        select(prod_code,unit,qty,po_name,lot,exp_date,actual_unit_cost,
+               actual_currency_code,delivery_date,warehouse_id,vendor_id,note)
+      
+      
+      
+      # check and remove existing entries
+      po_data <- check_exist(po_data,import_log, 
+                             check_col = c('prod_code','qty','lot','po_name'))
+      
+      # create a copy to update price, then drop existing entries
+      po_price <- po_data
+      po_data <- po_data[!po_data$exist,]
+      po_data$exist <- NULL
+      
+      
+      # writing to database
+      if (nrow(po_data)>0){
+        print('writing to import_log'); print(po_data)
+        db_write(config_dict,'import_log',po_data)
+        out_msg <- paste0(
+          out_msg, '\n',ui_elem$actual[ui_elem$label=='add_lotdate_success'])
       }
-      dbDisconnect(conn)
-      out_msg <- paste0(
-        out_msg, '\n',ui_elem$actual[ui_elem$label=='update_cost_success'])
+      
+      #update price
+      # keep only rows with price to prevent writing NA in database
+      po_price <- po_price[!is.na(po_price$actual_unit_cost),]
+      if (nrow(po_price)>0){
+        print('updating price(s)'); print(po_price)
+        conn <- db_open(config_dict)
+        for (i in 1:nrow(po_price)){
+          query <- paste0('update import_log set actual_unit_cost = ',
+                          po_price$actual_unit_cost[i],
+                          ' where po_name like ','"',po_price$po_name[i],'"',
+                          ' AND qty = ',po_price$qty[i], 
+                          ' AND lot like ','"',po_price$lot[i],'"',
+                          ' AND prod_code like ','"',po_price$prod_code[i],'"')
+          dbExecute(conn,query)
+        }
+        dbDisconnect(conn)
+        out_msg <- paste0(
+          out_msg, '\n',ui_elem$actual[ui_elem$label=='update_cost_success'])
+      }
+      if (out_msg==''){
+        out_msg <- ui_elem$actual[ui_elem$label=='nothing_to_update']
+      }
     }
-    if (out_msg==''){
-      out_msg <- ui_elem$actual[ui_elem$label=='nothing_to_update']
-    }
+    # reload import_log
+    reload_tbl(config_dict,'import_log')
+    
+    # show an alert
+    big_msg <- ui_elem$actual[ui_elem$label=='done']
+    shinyalert(title = big_msg, text = out_msg, type = "success")
   }
-  # reload import_log
-  reload_tbl(config_dict,'import_log')
-  
-  # show an alert
-  big_msg <- ui_elem$actual[ui_elem$label=='done']
-  shinyalert(title = big_msg, text = out_msg, type = "success")
 }
 
 # function to process the inv_in button
