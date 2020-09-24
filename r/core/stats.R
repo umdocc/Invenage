a722c06b8f351b0f6542ecb758d0ad3ee1c3e4be
umdocc <cuongcaodo@gmail.com>
2020-09-24 16:12:02 +0700

    Update stats.R
    rebuild get_sale_summary

--- r/core/stats.R
+++ r/core/stats.R
@@ -1,23 +1,22 @@
-# stats.R contains all function to extract data from database for report/stats purpose
-# it will mostly assume boot.R has been executed
+# stats.R contains all function used to summarise data
 
-# this function return the entire sale data, 
-# with all id/codes reaplaced with names/str
+# this function return the entire sale data summarise the data by prod_code,
+# providing all summary for each product
 get_sale_summary <- function(
-  vendor='all',from_date,group_cols = c('prod_code','customer_id')){
+  vendor_id=0, from_date=Sys.Date(), group_cols = c('prod_code','customer_id')){
   sale_data <- merge(
     sale_log,pxk_info %>% select(pxk_num,customer_id,sale_datetime))
   sale_data <- merge(sale_data, product_info %>% select(prod_code,vendor_id))
-  sale_data <- merge(sale_data, vendor_info %>% select(vendor,vendor_id))
   sale_data$total_price <- sale_data$unit_price*sale_data$qty
-  # filter the vendor if needed
-  if (vendor!='all'){
-    sale_data <- sale_data[grepl(vendor,sale_data$vendor),]
-  }
   
+  # filter the vendor if needed
+  if (vendor_id!=0){
+    sale_data <- sale_data[sale_data$vendor_id==vendor_id,]
+  }
+
   # filter using from_date
   sale_data <- sale_data[sale_data$sale_datetime>as.POSIXct(from_date),]
-  
+
   # convert to pack
   sale_data <- convert_to_pack(sale_data,packaging,'qty','pack_qty')
   
@@ -25,10 +24,6 @@ get_sale_summary <- function(
   sale_data <- sale_data %>% group_by_at(group_cols) %>%
     summarise(total_sale_pack = sum(pack_qty),.groups = 'drop')
 
-  # get the customer name
-  sale_data <- merge(sale_data,customer_info %>% 
-                       select(customer_id,customer_name))
-  
   # return
   return(sale_data)
 }
