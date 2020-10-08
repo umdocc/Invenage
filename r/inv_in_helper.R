622ce0f447a0df35c4e65173d770d45a7edc0d22
umdocc <cuongcaodo@gmail.com>
2020-10-08 09:46:11 +0700

    Update inv_in_helper.R
    add invalid prod_code detection while syncing po

--- r/inv_in_helper.R
+++ r/inv_in_helper.R
@@ -62,10 +62,27 @@ sync_po_to_db <- function(po_name){
   if(is.data.frame(po_data)){
     # add note columns if not presented in the po
     po_data <- add_missing_col(po_data,'note')
+    
     # merge and trim po_data to required columns
     po_data <- merge(po_data,product_info %>% select(ref_smn,vendor,prod_code),
                      all.x=T)
+    
+    # check the po for invalid prod_code and notify the user
+    invalid_ref <- po_data[is.na(po_data$prod_code),]
 
+    if(nrow(invalid_ref)>0){
+      # prepare the invalid items error message in text format
+      invalid_ref <- invalid_ref %>% 
+        mutate(name_ref=paste(name,ref_smn,sep = "-"))
+      invalid_ref <- paste(invalid_ref$name_ref,collapse = ";")
+      error_msg <- paste(get_actual("product"),invalid_ref,
+                         get_actual("not_found"))
+      
+      # display the message then do nothing
+      shinyalert(title = get_actual("error"),
+                 text = error_msg,
+                 type = "error")
+    }else{
     # write  the EXW price for po
     write_po_price(po_name,po_data)
     
@@ -94,8 +111,6 @@ sync_po_to_db <- function(po_name){
         select(prod_code,unit,qty,po_name,lot,exp_date,actual_unit_cost,
                actual_currency_code,delivery_date,warehouse_id,vendor_id,note)
       
-      
-      
       # check and remove existing entries
       po_data <- check_exist(po_data,import_log, 
                              check_col = c('prod_code','qty','lot','po_name'))
@@ -104,7 +119,6 @@ sync_po_to_db <- function(po_name){
       po_price <- po_data
       po_data <- po_data[!po_data$exist,]
       po_data$exist <- NULL
-      
       
       # writing to database
       if (nrow(po_data)>0){
@@ -143,6 +157,7 @@ sync_po_to_db <- function(po_name){
     # show an alert
     big_msg <- ui_elem$actual[ui_elem$label=='done']
     shinyalert(title = big_msg, text = out_msg, type = "success")
+    }
   }
 }
 
