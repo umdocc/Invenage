05fad5e7578834e9d8864978721ba2d1f18202d0
umdocc <cuongcaodo@gmail.com>
2021-06-20 16:31:33 +0700

    add support for rmd
    I like "knitr" better

--- admin1_ui/ui.R
+++ admin1_ui/ui.R
@@ -5,18 +5,25 @@ navbarPage(
   theme = shinytheme("united"), title = company_name, id = 'main',
   # actual ui object construction for each tab is located 
   # in <tabname>_tab.R file
+  
+  # sale menu
   navbarMenu(
     get_actual('inv_out'),
     cdn_tab,
     pxk_man_tab
   ),
+  
+  # import menu
   inv_in_tab,
+  
+  # reports menu
   navbarMenu(title = uielem$reports,
              po_inventory_tab,
              sale_log_report_tab,
              import_log_report_tab
              ),
-  # hr_log_tab,
+  
+  # update_db menu
   navbarMenu(
     get_actual('update_db'),
     update_customer_tab,
@@ -25,15 +32,21 @@ navbarPage(
     update_import_price_tab,
     update_vendor_tab
   ),
+  
+  # tools menu
   navbarMenu(
     get_actual('tools'),
     sync_excel_po_tab
   ),
+  
+  # service and warranty menu
   navbarMenu(
     get_actual('service_and_warranty'),
     tech_service_log_tab,
     tech_service_warranty_tab
   ),
+  
+  # ceo menu
   navbarMenu(
     get_actual('ceo'),
     vendor_import_performance_tab
