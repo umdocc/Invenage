f1bbc3dc6852c07ea150921dd20bf889027c9e99
umdocc <cuongcaodo@gmail.com>
2021-07-10 16:36:28 +0700

    rebuild everything

--- r/data_check.R
+++
@@ -1,8 +0,0 @@
-# various functions to check data requirements
-check_required_col <- function(data_df,required_col){
-  for (col_name in required_col){
-    if(!(col_name %in% names(data_df))){
-      stop(paste(col_name, 'column not found'))
-    }
-  }
-}
