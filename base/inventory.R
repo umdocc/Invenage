9bbf6d2ea9fce5d386bf3ef25324bbdde2215b83
umdocc <cuongcaodo@gmail.com>
2021-09-16 01:49:26 +0700

    Create inventory.R
    create inventory

---
+++ base/inventory.R
@@ -0,0 +1,13 @@
+get_sale_data <- function(vendor_id,from_date,to_date){
+  return(db_read_query(paste0(
+    "select sale_log.prod_code, sale_log.unit, sale_log.qty, 
+      pxk_info.sale_datetime, product_info.ref_smn
+      from sale_log inner join product_info 
+      on sale_log.prod_code=product_info.prod_code
+      inner join pxk_info
+      on sale_log.pxk_num = pxk_info.pxk_num
+      where vendor_id=",vendor_id," and
+      pxk_info.sale_datetime between '",from_date,
+      "' and '",to_date,"'"))
+  )
+}
