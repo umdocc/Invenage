d24da44b35b3ec521dfe7be9b1c92b9c4b32ef26
umdocc <cuongcaodo@gmail.com>
2021-12-30 14:24:28 +0700

    Update tool.R
    update some tool

--- base/tool.R
+++ base/tool.R
@@ -27,7 +27,8 @@ translate_tbl_column <- function(input_df,ui_elem=uielem){
 
 # write a single cell or a block to excel, using wb object, 
 # and a starting_coordinate 
-write_excel <- function(wb,written_data,start_cell_coor,sheet_num=1,split_char=";"){
+write_excel_sheet <- function(
+  wb, written_data, start_cell_coor="1;1",sheet_num=1,split_char=";"){
   if(grepl(split_char, start_cell_coor)){
     start_cell_coor <- as.numeric(
       unlist(strsplit(start_cell_coor,split=split_char)))
@@ -52,12 +53,35 @@ db_integrity_check <- function(){
 }
 
 # generate per customer pack prices by aggregating sale_log
-gen_customer_pricelist <- function(sale_data){
+gen_customer_pricelist <- function(
+  sale_data,group_vector=c("prod_code","customer_id"),
+  display_mode="full"){
+  
   data_df <- convert_to_pack(sale_data,packaging,"qty","pack_qty")
+  data_df <- data_df[!is.na(data_df$unit_price)&
+                       data_df$unit_price>=0,]
   data_df$pack_price <- data_df$unit_price*data_df$units_per_pack
-  data_df <- data_df %>% group_by(prod_code,customer_id) %>% 
+  data_df <- data_df %>% group_by_at(group_vector) %>% 
     summarise(min_price=min(pack_price),mid_price=median(pack_price),
               max_price=max(pack_price))
-  data_df <- merge(data_df,customer_info %>% select(customer_id,customer_name))
+  
+  # add display info if needed
+  if(display_mode=="full"){
+    # translate prod_code to comm_name, vendor and ref
+    data_df <- merge(
+      data_df,product_info %>% select(prod_code, comm_name, ref_smn),
+      all.x=T)
+    
+    # translate customer name if needed
+    if("customer_id" %in% group_vector){
+      data_df <- merge(
+        data_df,customer_info %>% select(customer_id,customer_name))
+    }
+  }
+  
+  # label for consistency
+  data_df$singular_price <- T
+  data_df$singular_price[data_df$min_price!=data_df$max_price] <- F
   return(data_df)
+  
 }
