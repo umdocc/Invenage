1da41bbf2612f2ab7a890ea12ce39acb83d25c89
umdocc <cuongcaodo@gmail.com>
2022-06-17 15:45:21 +0700

    Update slr_helper.R

--- report/slr_helper.R
+++ report/slr_helper.R
@@ -183,16 +183,16 @@ slr_del_line <- function(input, output){
 slr_edit_line <- function(input, output){
   tmp <- db_read_query("select label, actual from uielem")
   col_2edit <- tmp$label[tmp$actual==input$slr_pxk_line_col]
-  query <- paste0("update sale_log set ",col_2edit,"=",
+  query <- paste0("update sale_log set ",col_2edit,"='",
                   input$slr_pxk_line_col_content,
-                  " where id = ",input$slr_pxk_lineid)
+                  "' where id = ",input$slr_pxk_lineid)
   print(query)
-  # db_exec_query(query)
+  db_exec_query(query)
   gbl_load_tbl("sale_log")
   gbl_update_inventory()
-  slr_load_ui(
-    input,output,
-    c("slr_pxk_data"))
+  output <- slr_load_ui(
+    input,output, 
+    c('slr_data'))
   return(output)
 }
 
