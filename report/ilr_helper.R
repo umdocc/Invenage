8cd7c20f3ffe157036511d021a8a370972361515
umdocc <cuongcaodo@gmail.com>
2022-06-17 17:01:22 +0700

    working on ilr

--- report/ilr_helper.R
+++ report/ilr_helper.R
@@ -5,13 +5,17 @@ ilr_load_ui <- function(input,output,ui_list){
   if ('ilr_prod_filter' %in% ui_list){
     output$ilr_prod_filter <- render_ilr_prod_filter(input)
   }
+  if ('ilr_lineid' %in% ui_list){
+    output$ilr_lineid <- render_ilr_lineid(input)
+  }
+  
   return(output)
 }
 
 ilr_init <- function(input, output){
   ilr_load_ui(
     input,output,
-    c("ilr_data", "ilr_prod_filter"))
+    c("ilr_data", "ilr_prod_filter", "ilr_lineid"))
 }
 
 # render table for the pxk_man tab
@@ -60,6 +64,74 @@ get_ilr_data <- function(input, trans_col=T){
   
   return(output_tbl)
 }
+
+render_ilr_lineid <- function(input){renderUI({
+  lineid_list <- get_ilr_data(input)$id
+  selectizeInput(
+    inputId = "ilr_lineid", label = uielem$line_id,
+    choices = lineid_list,
+    selected = NULL,
+    options = list(create = F))
+  
+})
+}
+
+render_ilr_pxk_line_col <- function(input){renderUI({
+  # may add sophisticated filters later on
+  tmp <- db_read_query("select label, actual from uielem")
+  editable_col <- data.frame(
+    label = unlist(split_semi(config$ilr_editable_col)))
+  editable_col <- merge(editable_col, tmp)
+  editable_col <- editable_col$actual
+  selectizeInput(
+    inputId = "ilr_pxk_line_col", label = uielem$column,
+    choices = editable_col,
+    selected = editable_col[1],
+    options = list(create = F))
+  
+})
+}
+
+render_ilr_pxk_line_col_content <- function(input){renderUI({
+  selectizeInput(
+    inputId = "ilr_pxk_line_col_content", label = uielem$content,
+    choices = NULL,
+    selected = NULL,
+    options = list(create = T))
+  
+})
+}
+
+ilr_del_line <- function(input, output){
+  
+  query <- paste0("delete from sale_log where id = ", input$ilr_pxk_lineid)
+  # print(query)
+  db_exec_query(query)
+  gbl_load_tbl("sale_log")
+  gbl_update_inventory()
+  
+  output <- ilr_load_ui(
+    input,output, 
+    c('ilr_data'))
+  return(output)
+}
+
+ilr_edit_line <- function(input, output){
+  tmp <- db_read_query("select label, actual from uielem")
+  col_2edit <- tmp$label[tmp$actual==input$ilr_pxk_line_col]
+  query <- paste0("update sale_log set ",col_2edit,"='",
+                  input$ilr_pxk_line_col_content,
+                  "' where id = ",input$ilr_pxk_lineid)
+  print(query)
+  db_exec_query(query)
+  gbl_load_tbl("sale_log")
+  gbl_update_inventory()
+  output <- ilr_load_ui(
+    input,output, 
+    c('ilr_data'))
+  return(output)
+}
+
 
 ilr_print_report <- function(input, output){
   
--- report/ilr_tab.R
+++ report/ilr_tab.R
@@ -7,9 +7,22 @@ if('ilr' %in% hidden_tab){
     fluidRow(
       htmlOutput("ilr_prod_filter"),
       DT::dataTableOutput("ilr_data"),
-      actionButton(
-        inputId = "ilr_print_report",
-        label = uielem$print_report)
+      actionButton("ilr_print_report", uielem$print_report),
+      p(),
+      h4(uielem$edit_data),
+      div(style="display: inline-block;vertical-align:middle;width: 150px",
+          htmlOutput("ilr_lineid")),
+      div(style="display: inline-block;vertical-align:middle;width: 150px",
+          htmlOutput("ilr_pxk_line_col")),
+      div(style="display: inline-block;vertical-align:middle;width: 150px",
+          htmlOutput("ilr_line_col_content")),
+      p(),
+      div(style="display: inline-block;vertical-align:middle;width: 150px",
+          actionButton("ilr_del_line",uielem$del_line)),
+      p(),
+      div(style="display: inline-block;vertical-align:middle;width: 150px",
+          actionButton("ilr_edit_line",uielem$edit_data)),
+      p()
       )
   )
 }
