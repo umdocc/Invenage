c12e065406a7d2851529b063a6abd4bcf82df0ae
umdocc <cuongcaodo@gmail.com>
2022-06-20 12:35:48 +0700

    Update uvi_helper.R
    should work, not tested

--- db_update/uvi_helper.R
+++ db_update/uvi_helper.R
@@ -22,9 +22,11 @@ uvi_load_ui <- function(input,output,ui_list){
 }
 
 render_uvi_vendor <- function(){renderUI({
+  choice_list <- vendor_info$vendor
   selectizeInput(
     inputId = "uvi_vendor", label = uielem$vendor,
-    choices = NULL, selected = NULL)
+    choices = choice_list, selected = choice_list[1],
+    options = list(create = T))
 })}
 
 render_uvi_vendor_orig <- function(){renderUI({
@@ -46,5 +48,20 @@ render_uvi_vendor_local <- function(){renderUI({
 })}
 
 uvi_add_vendor <- function(input,output){
-  print(input$uvi_vendor_orig)
+  append_vendor <- data.frame(
+    vendor = input$uvi_vendor, orig_vendor = T, local = T)
+  
+  if(input$uvi_vendor_orig!=uielem$vendor){
+    append_vendor$orig_vendor <- F
+  }
+  if(input$uvi_vendor_local!=uielem$local){
+    append_vendor$local <- F
+  }
+  db_append_tbl("vendor_info",append_vendor)
+  # build the vendor code
+  new_vid <- db_read_query(paste0(
+    "select * from vendor_info where vendor='",
+    append_vendor$vendor,"'"))$vendor_id
+  update_query <- paste0("update vendor_info set vendor_code=concat('",
+                uvi_vendor_code_prefix,"',lpad(",new_vid)
 }
