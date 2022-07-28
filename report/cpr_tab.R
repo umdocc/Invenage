bb6c3ab5672bcda896a092c273b97b204ae9b958
umdocc <cuongcaodo@gmail.com>
2022-07-29 00:07:05 +0700

    Update cpr_tab.R
    simpler layout

--- report/cpr_tab.R
+++ report/cpr_tab.R
@@ -5,17 +5,8 @@ if('cpr' %in% hidden_tab){
   cpr_tab <- tabPanel(
     uielem$customer_price_report,
     fluidRow(
-      box(
-        width=3, height = 800,
-        p(),
         htmlOutput("cpr_customer"),
-        p()
-      ), # end left side
-      box(
-        width=9, height = 800,
-        p(),
         actionButton("cpr_create_report",uielem$print_report)
-      ) # end right side
     )
   )
 }
