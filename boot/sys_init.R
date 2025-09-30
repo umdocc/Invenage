fe96286c7e7565f603d39305904c4b0e0730a292
umdocc <cuongcaodo@gmail.com>
2025-09-30 10:43:41 +0700

    Update sys_init.R

--- boot/sys_init.R
+++ boot/sys_init.R
@@ -1,11 +1,13 @@
 # initilize database connection with just enough information for the
 # rest to follow
+rm(list=ls()) # boot clean-up
+options(warn=-1) # supress warnings
 
 # -------------------- sys init from local config ---------------------------
+library("DBI"); library("RMariaDB"); library(dplyr); library("tidyr")
 # path
 local_config_path <- file.path(path.expand("~/"),"appData","invenage","config.tsv")
 local_config <- read.table(local_config_path, header = T)
-
 
 # source boot helper for simple init
 source(file.path(local_config$value[local_config$name=="app_path"], "boot",
@@ -14,8 +16,15 @@ source(file.path(local_config$value[local_config$name=="app_path"], "boot",
 # re-load local config for integrity
 local_config <- load_local_config(local_config_path)
 
+# assign global variables
+assign("app_path", 
+       local_config$value[local_config$name=="app_path"],
+       envir=globalenv()
+       )
+
 # create the connection object
-require("DBI","RMariaDB")
+
+
 conn <- dbConnect(
   drv = RMariaDB::MariaDB(),
   username = local_config$value[local_config$name=='sql_usr'],
